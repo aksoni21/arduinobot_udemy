8b1d37031bc42f96cd2858617930c581d7aee43f
Ankur Soni <nkr.soni@gmail.com>
2024-05-21 14:51:57 -0400

    edited ard.launch.py to open jsp_gui

--- arduino_bot_desc/launch/ard.launch.py
+++ arduino_bot_desc/launch/ard.launch.py
@@ -22,8 +22,8 @@ def generate_launch_description():
     )
     
     joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher'
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui'
     )
 
     rviz_node = Node(
